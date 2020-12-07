a390bded17911d9df0b05a3b85ab7bad9014df2c
Mary Joy Castillo <35132948+Castillo14@users.noreply.github.com>
2020-12-07 18:31:22 +0800

    root

--- patient/config.php
+++ patient/config.php
@@ -1,7 +1,7 @@
 						<?php	$servername = "localhost";
-								$username = "root";
-								$password = "";
-								$dbname = "projectmms";
+								$username = "urafnghd_root";
+								$password = "reniertrenuela9";
+								$dbname = "urafnghd_moral";
 
 								// Create connection
 								$conn = new mysqli($servername, $username, $password, $dbname);
