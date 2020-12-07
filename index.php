99b7cbc73f619122d25fc09515b2534fd405e427
Mary Joy Castillo <35132948+Castillo14@users.noreply.github.com>
2020-12-07 17:24:59 +0800

    maps

--- index.php
+++ index.php
@@ -102,7 +102,7 @@
 <section class="map" id="footer">
     <div class="container">
     <h3 class="text-center"><br><br>Find us!</h3><br>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m21!1m12!1m3!1d3150.1829966583578!2d23.75232003459512!3d37.85600838634192!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m6!3e0!4m3!3m2!1d37.8566691!2d23.752137599999998!4m0!5e0!3m2!1sel!2sgr!4v1524459240043" style= "width:100%;  height:250px; border:0;" allowfullscreen></iframe>
+        <iframe src= div style="max-width:100%;overflow:hidden;color:red;width:500px;height:500px;"><div id="mymap-canvas" style="height:100%; width:100%;max-width:100%;"><iframe style="height:100%;width:100%;border:0;" frameborder="0" src="https://www.google.com/maps/embed/v1/place?q=saudi+arabia&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"></iframe></div><a class="embedded-maphtml" rel="nofollow" href="https://www.embed-map.com" id="authmap-data">https://www.embed-map.com</a><style>#mymap-canvas img{max-width:none!important;background:none!important;font-size: inherit;font-weight:inherit;}</style></div> style= "width:100%;  height:250px; border:0;" allowfullscreen></iframe>
     
         <div class="row staff">
             <div class="col">
