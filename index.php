42b681eebffca45904cad78eeefa646a9ebd0f57
Mary Joy Castillo <35132948+Castillo14@users.noreply.github.com>
2020-12-07 17:45:46 +0800

    style

--- index.php
+++ index.php
@@ -102,7 +102,7 @@
 <section class="map" id="footer">
     <div class="container">
     <h3 class="text-center"><br><br>Find us!</h3><br>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14950917.643677495!2d36.04780450236896!3d23.814104686216396!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x15e7b33fe7952a41%3A0x5960504bc21ab69b!2sSaudi%20Arabia!5e0!3m2!1sen!2sph!4v1607333559944!5m2!1sen!2sph" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14950917.643677495!2d36.04780450236896!3d23.814104686216396!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x15e7b33fe7952a41%3A0x5960504bc21ab69b!2sSaudi%20Arabia!5e0!3m2!1sen!2sph!4v1607333559944!5m2!1sen!2sph" aria-hidden="false" tabindex="0" style= "width:100%;  height:250px; border:0;" allowfullscreen></iframe>
     
         <div class="row staff">
             <div class="col">
